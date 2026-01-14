0d5ec552147e2168478d7a82bf78acef79828897
Phil G <p.gibson2003+jobs@gmail.com>
2026-01-14 16:16:09 +0000

    refactor(S1): Data Access Layer
    - Inject data access into controller
    - Converted the HTTP GET route to utilise the injected service
    - Reference API from test project
    - Added controller tests to validate DI behaviour

--- Greggs.Products.UnitTests/UnitTest1.cs
+++ Greggs.Products.UnitTests/UnitTest1.cs
@@ -1,13 +1,62 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Greggs.Products.Api.Controllers;
+using Greggs.Products.Api.DataAccess;
+using Greggs.Products.Api.Models;
+using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
 
 namespace Greggs.Products.UnitTests;
 
-public class UnitTest1
+public class ProductControllerTests
 {
     [Fact]
-    public void Test1()
+    public void Get_ReturnsProductsFromDataAccess()
     {
-        throw new NotImplementedException("We have no tests :-(");
+        var expected = new List<Product>
+        {
+            new() { Name = "Test Product", PriceInPounds = 1.25m }
+        };
+        var dataAccess = new FakeProductAccess(expected);
+        var controller = new ProductController(NullLogger<ProductController>.Instance, dataAccess);
+
+        var result = controller.Get(0, 5).ToList();
+
+        Assert.Single(result);
+        Assert.Equal(expected[0], result[0]);
+    }
+
+    [Fact]
+    public void Get_PassesPagingToDataAccess()
+    {
+        var dataAccess = new FakeProductAccess(Array.Empty<Product>());
+        var controller = new ProductController(NullLogger<ProductController>.Instance, dataAccess);
+
+        // Discard the result of this for the analyser. We do not need
+        // the output, but we want to force enumeration.
+        _ = controller.Get(2, 3).ToList();
+
+        Assert.Equal(2, dataAccess.PageStart);
+        Assert.Equal(3, dataAccess.PageSize);
+    }
+
+    private sealed class FakeProductAccess : IDataAccess<Product>
+    {
+        public int? PageStart { get; private set; }
+        public int? PageSize { get; private set; }
+        private readonly IEnumerable<Product> _result;
+
+        public FakeProductAccess(IEnumerable<Product> result)
+        {
+            _result = result;
+        }
+
+        public IEnumerable<Product> List(int? pageStart, int? pageSize)
+        {
+            PageStart = pageStart;
+            PageSize = pageSize;
+            return _result;
+        }
     }
 }
